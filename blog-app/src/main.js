68799b5de49162190ed69d0cecb9785b7a964a43
yixiaoi <zhangyixiao0311@gmail.com>
2024-08-06 10:34:10 +0800

    lmessage vue

--- blog-app/src/main.js
+++ blog-app/src/main.js
@@ -6,12 +6,17 @@ import router from './router'
 import MyNavigation from './components/Navigation.vue'
 import MyArticleList from './components/Articlelist.vue'
 import MyAsideList from './components/Asidelist.vue'
+import LmessageList from './views/LmessageList.vue';
+import LmessageEditor from './views/LmessageEditor.vue';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.component('my-nav', MyNavigation);
 Vue.component('my-art-list', MyArticleList);
 Vue.component('my-asi-list', MyAsideList)
+Vue.component('my-lme-list',LmessageList)
+Vue.component('my-lme-editor',LmessageEditor)
+
 
 
 new Vue({
--- blog-app/src/views/LmessageView.vue
+++ blog-app/src/views/LmessageView.vue
@@ -2,14 +2,18 @@
     <div>
         <my-nav></my-nav>
         <el-container>
-            <el-aside width="200px">Aside</el-aside>
-            <el-main>lmessage</el-main>
+            <el-header></el-header>
+            <lmessage-editor></lmessage-editor>
+            <lmessage-list></lmessage-list>
         </el-container>
     </div>
 </template>
 
 <script>
+import LmessageEditor from './LmessageEditor.vue'
+import LmessageList from './LmessageList.vue'
 export default {
+  components: { LmessageEditor, LmessageList },
     
 }
 </script>
